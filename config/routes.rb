6a93dfd2e1d94736a7fba53d4c379ac255ac8d89
Paulina Staszewska <106317612+PauStasz@users.noreply.github.com>
2023-05-23 04:14:25 +0200

    bin i config

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  root to: "public#home"
+  get 'public/home'
+  authenticated :user do
+    resources :profile
+    resources :date
+    resources :training 
+    post '/training/:id', to: 'training#create', as: 'create_training'
+    resources :ranking
+  end
+  
+  devise_for :users, controllers: 
+  { 
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+end
